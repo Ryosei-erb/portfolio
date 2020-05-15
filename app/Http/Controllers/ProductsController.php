f11a79a062c254d9737893e6ff41ea55df934ebb
Ryosei-erb <ryoseiyoshikawa@yoshikawaryouseinoMacBook-Pro.local>
2020-05-15 14:32:05 +0900

    introduce googlemaps api

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -9,6 +9,7 @@ use App\Favorite;
 use Illuminate\Support\Facades\Auth;
 use Intervention\Image\Facades\Image;
 use Illuminate\Http\Request;
+use "GoogleMaps";
 
 class ProductsController extends Controller
 {
