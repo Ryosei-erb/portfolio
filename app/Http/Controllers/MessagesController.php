d7af247a7465597a454babd01101b73e24c7638d
Ryosei-erb <ryoseiyoshikawa@yoshikawaryouseinoMacBook-Pro.local>
2020-05-15 12:03:11 +0900

    add the show in message

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Message;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class MessagesController extends Controller
 {
-    //
+    public function store(Request $request) {
+        $this->validate($request, Message::$rules);
+        Message::create([
+            "user_id" => Auth::user()->id,
+            "room_id" => $request->room_id,
+            "content" => $request->content
+        ]);
+        return redirect("/rooms/" . $request->room_id);
+    }
 }
