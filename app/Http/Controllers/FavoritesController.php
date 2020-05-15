9a1ccaec53665e8c092beeaa5c2d977857d5aa04
Ryosei-erb <ryoseiyoshikawa@yoshikawaryouseinoMacBook-Pro.local>
2020-05-15 12:20:19 +0900

    add the favorites controller

--- app/Http/Controllers/FavoritesController.php
+++ app/Http/Controllers/FavoritesController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Favorite;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class FavoritesController extends Controller
 {
+    public function store(Request $request) {
+      Favorite::create(["user_id" => Auth::user()->id, "product_id" => $request->product_id]);
+      return redirect("/products/" . $request->product_id);
+    }
 
+    public function update(Request $request, $id) {
+      $favorite = Favorite::find($id);
+      $favorite->delete();
+      return redirect("/products/" . $request->product_id);
 }
