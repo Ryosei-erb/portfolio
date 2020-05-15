614bc5f84abcc13f0991caf7663712a8bb67a76f
Ryosei-erb <ryoseiyoshikawa@yoshikawaryouseinoMacBook-Pro.local>
2020-05-15 10:59:21 +0900

    make  and validation in product

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $guarded = array("id");
+    protected $table = "products";
+    protected $fillable = ["name", "description", "pickup_times", "price", "user_id", "image", "address"];
+    public static $rules = array(
+      "name" => "required|string|",
+      "description" => "required|string",
+      "pickup_times" => "required|string",
+      "price" => "required|integer",
+    );
 }
