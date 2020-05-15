42ab28e970d14180cfc33dece3d594d3e96a8fbb
Ryosei-erb <ryoseiyoshikawa@yoshikawaryouseinoMacBook-Pro.local>
2020-05-15 11:05:34 +0900

    add create and store action in product

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Product;
 use Illuminate\Http\Request;
 
 class ProductsController extends Controller
 {
-    //
+    public function create() {
+        return view("product.create");
+    }
+
+    public function store(Request $request) {
+        $this->validate($request, Product::$rules);
+        $product = new Product;
+
+        $product->name = $request->name;
+        $product->description = $request->description;
+        $product->pickup_times = $request->pickup_times;
+        $product->price = $request->price;
+        $product->image = $filename;
+        $product->address = $request->address;
+        $product->save();
+        return redirect("/products");
+    }
 }
